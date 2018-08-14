d89d5521c6639bd08b90406791e7b6f6eee138b4
Arnold Daniels <arnold@jasny.net>
2018-08-15 01:10:10 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+Jasny skeleton library (PHP)
+===
+
+Super easy to create a new Jasny library.
+
+_Note: It's really specific and will only properly work for our organization Jasny. If you want to use it, fork and modify init.sh._
+
+Usage
+---
+
+    composer create-project jasny/library
+
---
+++ init.sh
@@ -0,0 +1,47 @@
+REPO_NAME=$(pwd | xargs basename)
+
+SCRUTINIZER_ORGANIZATION=jasny
+SCRUTINIZER_GLOBAL_CONFIG=9fc4e5aa-b4a6-4b2b-b698-9a17549e1ddc
+
+mv README.md.dist README.md
+sed -i 's~{{library}}~'$REPO_NAME'~' README.md
+sed -i 's~jasny/library~jasny/'$REPO_NAME'~' composer.json
+sed -i 's~Jasny\\Library~Jasny\\'$(echo $REPO_NAME | sed -r 's/(^|-)(\w)/\U\2/g')'~' composer.json
+
+mkdir -p src tests
+composer install
+
+cp vendor/jasny/php-code-quality/.gitignore .
+cp vendor/jasny/php-code-quality/phpunit.xml.dist .
+cp vendor/jasny/php-code-quality/phpcs.xml.dist ./phpcs.xml
+cp vendor/jasny/php-code-quality/phpstan.neon.dist ./phpstan.neon
+cp vendor/jasny/php-code-quality/travis.yml.dist ./.travis.yml
+cp vendor/jasny/php-code-quality/bettercodehub.yml.dist ./.bettercodehub.yml
+
+git add .
+git commit -a -m "Initial commit"
+git push
+
+# Travis
+command -v travis || gem install travis --no-rdoc --no-ri
+travis sync && travis enable
+
+# Scrutinizer
+if [ -n "$SCRUTINIZER_ACCESS_TOKEN" ] ; then
+  echo "Skipping scrutinizer: access token not configured"
+else
+  curl --header "Content-Type: application/json" --request POST \
+    --data "{\"name\":\"jasny/$REPO_NAME\",\"organization\":\"$SCRUTINIZER_ORGANIZATION\",\"global_config\"=\"$SCRUTINIZER_GLOBAL_CONFIG\"}" \
+    https://scrutinizer-ci.com/api/repositories/g?access_token="$SCRUTINIZER_ACCESS_TOKEN"
+fi
+
+# TODO use sensiolabs API or CLI
+xdg-open https://insight.sensiolabs.com/projects/new/github
+
+# Better code hub doesn't have an API
+echo "Enable BetterCodeHub via https://bettercodehub.com/repositories"
+
+# Edit README
+EDIT=$(command -v xdg-open && echo 'xdg-open' || echo 'open')
+$EDIT README.md
+
